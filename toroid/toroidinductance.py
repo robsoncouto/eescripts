9dd401d96eb3bd9a8e5c525ad163b152eb4ad6cb
Robson <robsoncouto@outlook.com>
2018-03-03 12:01:33 -0300

    Fixed script for calculating the toroid inductance

--- toroid/toroidinductance.py
+++ toroid/toroidinductance.py
@@ -1,27 +1,28 @@
 import math
+import sys
 #http://hyperphysics.phy-astr.gsu.edu/hbase/magnetic/indtor.html
 print("Calculation of the Approximate Inductance of a Toroid\n")
-ans=input("Is the cross sectional area of the toroid round(1) or rectangular(2)?\n")
-print(ans,type(ans))
+ans=int(input("Is the cross sectional area of the toroid round(1) or rectangular(2)?\n"))
 
 if ans==1:
-    ans=input("Please input the diameter of the cross section in cm\n")
-    area=(2*math.pi)*(ans/2)
+    ans=float(input("Please input the diameter of the cross section in cm\n"))/2*0.01 #radius cm to meters
+    area=(math.pi)*(ans)(ans)
     print(area)
-else:    
-    ans=input("Please input the height of the cross section in cm\n")
+elif ans==2:    
+    ans=float(input("Please input the height of the cross section in cm\n"))*0.01#cm to meters
     area=ans
-    ans=input("Please input the width the cross section in cm\n")
-    area=area*area
-    print(area)
+    ans=float(input("Please input the width the cross section in cm\n"))*0.01#cm to meters
+    area=area*ans
+    print("Area: {:f}m2".format(area))
+else :
+    print("Undefined option, please input 1 or 2")
+    sys.exit(1)
 
-ans=input("Please input the diameter of the toroid, use the centerline\n")
-radius=ans/2
-ans=input("Please input the number of coils\n")
-N=ans/2
+radius=float(input("Please input the diameter of the toroid, use the centerline\n"))/2*0.01#cm to meters
+N=int(input("Please input the number of coils\n"))
 
-ans=input("Please input Relative permeability of the core.\n")
-u=ans/2
+ur=float(input("Please input Relative permeability of the core.\n"))
+u=ur*0.00000126
 
 L=(u*(N*N)*area)/(2*math.pi*radius)
 print("The Approximate Inductance of this Toroid is:\n")
